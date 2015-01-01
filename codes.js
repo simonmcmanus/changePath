79ccb4d13402b4c538209f25daedee80f922f869
Simon McManus <mcmanus.simon@gmail.com>
2015-01-01 11:14:15 +0000

    added travis

---
+++ codes.js
@@ -0,0 +1,10 @@
+module.exports = {
+    PROP_CREATE: 'PROP_CREATE',
+    PROP_UPDATE: 'PROP_UPDATE',
+    PROP_DELETE: 'PROP_DELETE',
+    ITEM_CREATE: 'ITEM_CREATE',
+//    ITEM_UPDATE: 'ITEM_UPDATE', atm arrays can only be objects so this is not used.
+    ITEM_DELETE: 'ITEM_DELETE',
+    ITEM_MOVE: 'ITEM_MOVE',
+    ITEMS_SWAPPED: 'ITEMS_SWAPPED'
+};
